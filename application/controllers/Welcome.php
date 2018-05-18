3acbb55c0a32e26f15e391e3f53575a8fdcbfff8
megabreakage <martin.njau@live.com>
2018-05-19 00:17:41 +0300

    Activates index function to load all the products and view function by id

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,27 +3,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
+	public function index()	{
+
+		$data['products'] = $this->products_model->get_products();
+
+		$this->load->view('templates/header');
+		$this->load->view('products', $data);
+		$this->load->view('templates/footer');
 	}
 
-	public function view(){
-		$this->load->view('welcome_message');
+	public function view($id){
+		$data['product'] = $this->products_model->get_product($id);
+
+		// echo '<pre>';
+		// 	echo print_r($data);
+		// echo '</pre>';
+		// exit();
+
+		$this->load->view('templates/header');
+		$this->load->view('product_view', $data);
+		$this->load->view('templates/footer');
 	}
+
 }
+
+?>
